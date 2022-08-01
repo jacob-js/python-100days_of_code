d42c2e6b90b1f1260cd85f4f1387d9c857f63dd8
Jacob Dev <mercihabam@gmail.com>
2022-08-01 20:14:25 +0200

    add decode function

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -2,7 +2,11 @@ import string
 
 
 alphabets = list(string.ascii_lowercase)
-action = input("Enter the action to execute. d for decode and e for encoding \n")
+action = input("Enter the action to execute. 'd' to decode and e for encoding \n")
+
+while action not in ("d", "e"):
+    action = input("Please enter a valid action letter. 'd' to decode and e for encoding \n")
+
 message = input("Type your message : \n")
 shift = int(input("Shift number : \n"))
 
@@ -14,6 +18,20 @@ def encode_msg():
         except IndexError:
             rel = alphabets[alphabets.index(l)-shift]
         crypted += rel
-    print(crypted)
+    print(f"The encoded message is : {crypted}")
 
-encode_msg()
+def decode_msg():
+    decoded = ""
+    for l in message:
+        try:
+            rel = alphabets[alphabets.index(l)-shift]
+        except IndexError:
+            rel = alphabets[alphabets.index(l)+shift]
+        decoded += rel
+    
+    print(f"The decoded message is : {decoded}")
+
+if action == "d":
+    decode_msg()
+else:
+    encode_msg()
